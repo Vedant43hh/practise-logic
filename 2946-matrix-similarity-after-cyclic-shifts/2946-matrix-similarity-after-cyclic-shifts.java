2300078eaab4d1ca76f6ec79a36d5e62e03dba77
Vedant <vedantved2357@gmail.com>
2026-08-22 22:16:36 +0530

    Time: 1 ms (99.02%), Space: 47.1 MB (31.8%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -3,17 +3,15 @@ class Solution {
         int m = mat.length;
         int n = mat[0].length;
 
-        k %= n; //(reduce k<n)
+        k %= n; 
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (i % 2 == 0) {
-                    // even row , left shift
                     if (mat[i][j] != mat[i][(j + k) % n]) {
                         return false;
                     }
                 } else {
-                    // odd row , right shift
                     if (mat[i][j] != mat[i][(j - k + n) % n]) {
                         return false;
                     }
